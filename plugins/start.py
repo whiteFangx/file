12837f2a0c62c793b0de5577b7f2ec9e05666bd1
Lucifer6989 <91875644+Lucifer6989@users.noreply.github.com>
2023-01-08 10:40:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -101,9 +101,8 @@ async def start_command(client: Client, message: Message):
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,parse_mode=PARSEMODE.HTML
-            quote = True
+            reply_markup = InlineKeyboardMarkup(buttons), quote = True, disable_web_page_preview = True,
+parse_mode=Parsemode.HTML
         )
         return
 
